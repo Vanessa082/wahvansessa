c77a981fdeaf6edb6fca699262faaac71d09d2e3
vanessa082 <wahvanessa22@gmail.com>
2024-09-18 23:32:26 +0100

    added a button on the navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar () {
+  const navItems
+  return(
+    <header>
+      <h1 className="">Wah</h1>
+      <nav>
+
+      </nav>
+        <button>Contact</button>
+    </header>
+  )
+}
