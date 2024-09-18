07fa5a89b53a7f0aa3566e6c6060cde333f07729
vanessa082 <wahvanessa22@gmail.com>
2024-09-18 23:35:49 +0100

    added an array to handle nav items

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,22 @@
 export default function Navbar () {
-  const navItems
+  const navItems = [
+    {
+      text: "About",
+      href: "#"
+    },
+    {
+      text: "Projects",
+      href: "#"
+    },
+    {
+      text: "Certifications",
+      href: "#"
+    },
+    {
+      text: "Blog",
+      href: "#"
+    }
+  ]
   return(
     <header>
       <h1 className="">Wah</h1>
